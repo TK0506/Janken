e3a536c36fc0941388e109091080597bb49da668
Takanori Ikegawa <takanoriikegawa@takanorinomacbook-puro.local>
2019-01-25 10:44:59 +0900

    Initial Commit

---
+++ JankenG's/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  JankenG's
+//
+//  Created by Takanori Ikegawa on 2019/01/25.
+//  Copyright © 2019年 Takanori Ikegawa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
